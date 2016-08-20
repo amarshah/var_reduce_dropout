fe7c531da8cb2ef558c815b8301581e6baf6b140
amarshah <amarshah793@gmail.com>
2016-08-20 19:23:37 +0000

    adding a script with 2 iid dropout masks

--- mnist_2samples.py
+++ mnist_2samples.py
@@ -41,19 +41,24 @@ Y_test = np_utils.to_categorical(y_test, n_classes)
 # specify model
 x = Input(batch_shape=(n_batch, n_in))
 maskx = K.dropout(K.ones((n_batch, n_in)), p)
+maskx2 = K.dropout(K.ones((n_batch, n_in)), p)
 # print((1. / p - maskx).eval())
 
 layer1 = Dense(1024, activation='relu')
 mask1 = K.dropout(K.ones((n_batch, n_layer)), p)
+mask5 = K.dropout(K.ones((n_batch, n_layer)), p)
 
 layer2 = Dense(1024, activation='relu')
 mask2 = K.dropout(K.ones((n_batch, n_layer)), p)
+mask6 = K.dropout(K.ones((n_batch, n_layer)), p)
 
 layer3 = Dense(1024, activation='relu')
 mask3 = K.dropout(K.ones((n_batch, n_layer)), p)
+mask7 = K.dropout(K.ones((n_batch, n_layer)), p)
 
 layer4 = Dense(1024, activation='relu')
 mask4 = K.dropout(K.ones((n_batch, n_layer)), p)
+mask8 = K.dropout(K.ones((n_batch, n_layer)), p)
 
 softmax_layer = Dense(n_out, activation='softmax')
 
@@ -64,11 +69,11 @@ dropout_layer2 = Lambda(lambda x: x * mask2, output_shape = (n_layer,))
 dropout_layer3 = Lambda(lambda x: x * mask3, output_shape = (n_layer,))
 dropout_layer4 = Lambda(lambda x: x * mask4, output_shape = (n_layer,))
 
-dropout_neg_in = Lambda(lambda x: x * (1. / p - maskx), output_shape=(n_in,))
-dropout_neg_layer1 = Lambda(lambda x: x * (1. / p - mask1), output_shape = (n_layer,))
-dropout_neg_layer2 = Lambda(lambda x: x * (1. / p - mask2), output_shape = (n_layer,))
-dropout_neg_layer3 = Lambda(lambda x: x * (1. / p - mask3), output_shape = (n_layer,))
-dropout_neg_layer4 = Lambda(lambda x: x * (1. / p - mask4), output_shape = (n_layer,))
+dropout_2_in = Lambda(lambda x: x * maskx2, output_shape=(n_in,))
+dropout_2_layer1 = Lambda(lambda x: x * mask5, output_shape = (n_layer,))
+dropout_2_layer2 = Lambda(lambda x: x * mask6, output_shape = (n_layer,))
+dropout_2_layer3 = Lambda(lambda x: x * mask7, output_shape = (n_layer,))
+dropout_2_layer4 = Lambda(lambda x: x * mask8, output_shape = (n_layer,))
 
 
 # apply model
@@ -78,11 +83,11 @@ out1 = layer3(dropout_layer2(out1))
 out1 = layer4(dropout_layer3(out1))
 out1 = softmax_layer(dropout_layer4(out1))
 
-out2 = layer1(dropout_neg_in(x))
-out2 = layer2(dropout_neg_layer1(out2))
-out2 = layer3(dropout_neg_layer2(out2))
-out2 = layer4(dropout_neg_layer3(out2))
-out2 = softmax_layer(dropout_neg_layer4(out2))
+out2 = layer1(dropout_2_in(x))
+out2 = layer2(dropout_2_layer1(out2))
+out2 = layer3(dropout_2_layer2(out2))
+out2 = layer4(dropout_2_layer3(out2))
+out2 = softmax_layer(dropout_2_layer4(out2))
 
 avg = Lambda(lambda args: 0.5 * (args[0] + args[1]), output_shape=(n_out,))
 prediction = avg([out1, out2])
