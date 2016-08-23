4a25399e25e4e75e40e3bde69e09f68856e0c9ba
amarshah <amarshah793@gmail.com>
2016-08-23 13:52:37 +0000

    adding batch norm

--- models.py
+++ models.py
@@ -3,6 +3,7 @@
 from keras.callbacks import Callback
 from keras import backend as K
 from keras.layers import Input, Dense, Lambda
+from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 from keras.utils import np_utils
 import time
@@ -47,12 +48,17 @@ def evaluate(model, X, Y, n_batch, n_mc):
 
 	return score
 
+def apply_layer(input, layer, batch_norm, dropout_mask = None):
+	if dropout_mask is None:
+		output = layer(input)
+	else:
+		output = layer(dropout_mask(input))
 
-def run_model(n_batch, n_in, n_layer, n_out, n_epoch,
-	      p, dropout_masks, n_mc,
-	      X_train, Y_train, X_test, Y_test):
+	if batch_norm: output = BatchNormalization()(output)
 
-	# specify model
+	return output
+
+def define_model(n_in, n_layer, n_out, p, dropout_flag, batch_norm):
 	# x = Input(batch_shape=(n_batch, n_in))
 	x = Input(shape=(n_in,))
 	maskx = K.dropout(K.ones((n_in,)), p)
@@ -72,6 +78,18 @@ def run_model(n_batch, n_in, n_layer, n_out, n_epoch,
 
 	softmax_layer = Dense(n_out, activation='softmax')
 
+	if dropout_flag == -1:
+		maskx2 = (1. / p - maskx)
+		mask5 = (1. / p - mask1)
+		mask6 = (1. / p - mask2)
+		mask7 = (1. / p - mask3)
+		mask8 = (1. / p - mask4)
+	elif dropout_flag == 2:
+		maskx2 = K.dropout(K.ones((n_in)), p)
+		mask5 = K.dropout(K.ones((n_layer,)), p)
+		mask6 = K.dropout(K.ones((n_layer,)), p)
+		mask7 = K.dropout(K.ones((n_layer,)), p)
+		mask8 = K.dropout(K.ones((n_layer,)), p)
 
 	dropout_in = Lambda(lambda x: x * maskx, output_shape=(n_in,))
 	dropout_layer1 = Lambda(lambda x: x * mask1, output_shape = (n_layer,))
@@ -79,51 +97,41 @@ def run_model(n_batch, n_in, n_layer, n_out, n_epoch,
 	dropout_layer3 = Lambda(lambda x: x * mask3, output_shape = (n_layer,))
 	dropout_layer4 = Lambda(lambda x: x * mask4, output_shape = (n_layer,))
 
-	if dropout_masks == -1:
-		maskx2 = (1. / p - maskx)
-		mask5 = (1. / p - mask1)
-		mask6 = (1. / p - mask2)
-		mask7 = (1. / p - mask3)
-		mask8 = (1. / p - mask4)
-	elif dropout_masks == 2:
-		maskx2 = K.dropout(K.ones((n_in)), p)
-		mask5 = K.dropout(K.ones((n_layer,)), p)
-		mask6 = K.dropout(K.ones((n_layer,)), p)
-		mask7 = K.dropout(K.ones((n_layer,)), p)
-		mask8 = K.dropout(K.ones((n_layer,)), p)
-	elif dropout_masks == 1:
-		maskx2 = maskx
-		mask5 = mask1
-		mask6 = mask2
-		mask7 = mask3
-		mask8 = mask4
-
-	dropout_neg_in = Lambda(lambda x: x * maskx2, output_shape=(n_in,))
-	dropout_neg_layer1 = Lambda(lambda x: x * mask5, output_shape = (n_layer,))
-	dropout_neg_layer2 = Lambda(lambda x: x * mask6, output_shape = (n_layer,))
-	dropout_neg_layer3 = Lambda(lambda x: x * mask7, output_shape = (n_layer,))
-	dropout_neg_layer4 = Lambda(lambda x: x * mask8, output_shape = (n_layer,))
+	if dropout_flag != 1:
+		dropout_neg_in = Lambda(lambda x: x * maskx2, output_shape=(n_in,))
+		dropout_neg_layer1 = Lambda(lambda x: x * mask5, output_shape = (n_layer,))
+		dropout_neg_layer2 = Lambda(lambda x: x * mask6, output_shape = (n_layer,))
+		dropout_neg_layer3 = Lambda(lambda x: x * mask7, output_shape = (n_layer,))
+		dropout_neg_layer4 = Lambda(lambda x: x * mask8, output_shape = (n_layer,))
 
 	# apply model
-	out1 = layer1(dropout_in(x))
-	out1 = layer2(dropout_layer1(out1))
-	out1 = layer3(dropout_layer2(out1))
-	out1 = layer4(dropout_layer3(out1))
-	out1 = softmax_layer(dropout_layer4(out1))
+	out1 = apply_layer(x, layer1, batch_norm, dropout_in)
+	out1 = apply_layer(out1, layer2, batch_norm, dropout_layer1)
+	out1 = apply_layer(out1, layer3, batch_norm, dropout_layer2)
+	out1 = apply_layer(out1, layer4, batch_norm, dropout_layer3)
+	out1 = apply_layer(out1, softmax_layer, batch_norm, dropout_layer4)
 
-	out2 = layer1(dropout_neg_in(x))
-	out2 = layer2(dropout_neg_layer1(out2))
-	out2 = layer3(dropout_neg_layer2(out2))
-	out2 = layer4(dropout_neg_layer3(out2))
-	out2 = softmax_layer(dropout_neg_layer4(out2))
+	if dropout_flag != 1:
+		out2 = apply_layer(x, layer1, batch_norm, dropout_neg_in)
+		out2 = apply_layer(out2, layer2, batch_norm, dropout_neg_layer1)
+		out2 = apply_layer(out2, layer3, batch_norm, dropout_neg_layer2)
+		out2 = apply_layer(out2, layer4, batch_norm, dropout_neg_layer3)
+		out2 = apply_layer(out2, softmax_layer, batch_norm, dropout_neg_layer4)
 
-	if dropout_masks==1:
-		prediction = out1
-	else:
 		avg = Lambda(lambda args: 0.5 * (args[0] + args[1]), output_shape=(n_out,))
 		prediction = avg([out1, out2])
+	else:
+		prediction = out1
 
 	model = Model(input=x, output=prediction)
+
+	return model
+
+
+def run_model(n_in, n_layer, n_out, p, dropout_flag, batch_norm,
+	n_batch, n_epoch, n_mc, X_train, Y_train, X_test, Y_test):
+
+	model = define_model(n_in, n_layer, n_out, p, dropout_flag, batch_norm)
 
 	model.compile(optimizer='adam',
 	              loss='categorical_crossentropy',
@@ -139,13 +147,14 @@ def run_model(n_batch, n_in, n_layer, n_out, n_epoch,
 	# history = epoch_history.history
 	# score = model.evaluate(X_test, Y_test, batch_size=n_batch, verbose=0)
 
-	# initial train and test loss
+	# initial losses
 	out = model.evaluate(X_train, Y_train, batch_size=n_batch)
 	train_losses = [out[0]]
 	out = model.evaluate(X_test, Y_test, batch_size=n_batch)
 	test_losses = [out[0]]
 	train_times = [0]
 
+	# train the model
 	for e in xrange(n_epoch):
 		for batch_ind in xrange(len(X_train) / n_batch):
 			batch = range(batch_ind*n_batch, (batch_ind+1)*n_batch)
